334f7a398504eb26650a7ed75b381b1c6d3185f8
Kein <kein.1945@gmail.com>
2012-04-06 15:13:45 +0400

    Modifed cil protocol packet manager

--- js/cil.protocol.js
+++ js/cil.protocol.js
@@ -1,32 +1,44 @@
 
-var cil_protocol = {
-	encoder : function(){
-		this.encode = function(data, channel){
-			if( !CLASS.instanceOf( data, cil_protocol.packetInterface ) )
-				return data
-			channel.write( data.getId() )
-			data.send(channel)
+var cil_protocol = (function(){
+	var availablePackets = []
+		, isPacketExists = function(id){
+			return typeof(availablePackets[id]) != "undefined"
 		}
-	}
-	, decoder: function(){
-		this.decode = function(channel){
-			var id = channel.readInt()
-			for(var index in cil_protocol.availablePackets){
-				if( index == id ){
-					var packet = new cil_protocol.availablePackets[index]()
+		, getPacketById = function(id){
+			return new (availablePackets[id])()
+		}
+	return {
+		encoder : function(){
+			this.encode = function(data, channel){
+				if( !CLASS.instanceOf( data, cil_protocol.packetInterface ) )
+					return data
+				channel.write( data.getId() )
+				data.send(channel)
+			}
+		}
+		, decoder: function(){
+			this.decode = function(channel){
+				var id = channel.readInt()
+				if( isPacketExists(id) ){
+					var packet = getPacketById(id)
 					packet.get(channel)
 					return packet
+				} else {
+					throw "Failed to encode packet with id "+id
+					return false
 				}
 			}
-			throw "Failed to encode packet with id "+id
-			return false
+		}
+		, isPacketExists: isPacketExists
+		, getPacketById: getPacketById
+		, packetInterface : {
+			getId : function(){}
+			, getName: function(){}
+			, send : function(channel){}
+			, get : function(channel){}
+		}
+		, addPacketPrototype: function(id, packetPrototype){
+			return availablePackets[id] = packetPrototype
 		}
 	}
-	, packetInterface : {
-		getId : function(){}
-		, getName: function(){}
-		, send : function(channel){}
-		, get : function(channel){}
-	}
-	, availablePackets : []
-}
+})()
